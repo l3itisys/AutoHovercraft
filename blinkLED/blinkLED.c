0ad41b35d16c17b63841f4263604f79f277e8b76
l3itisys <leiticia.tlb@gmail.com>
2023-05-25 18:17:53 -0400

    LED test

--- blinkLED/blinkLED.c
+++ blinkLED/blinkLED.c
@@ -2,13 +2,15 @@
 #include <util/delay.h> 
 
 int main(void) {
-  DDRB |= 0b00000001; 
+  DDRB |= 0b00000001; /* Data Direction Register B: 
+                         writing a 1 to the bit enables output. */ 
 
+  // ----- Event loop ----- // 
   while (1) {
-    PORTB = 0b00000001; 
-    _delay_ms(1000); 
+    PORTB = 0b00000001; /* Turn on the first LED bit/pin in PORTB */ 
+    _delay_ms(1000);    /* wait */ 
 
-    PORTB = 0b00000000; 
+    PORTB = 0b00000000; /* Turn off all B pins, including LED */ 
     _delay_ms(1000); 
 
   }
